ba360da354dcb816971f65f7078fef8345c0fc8b
Steve Cosman <scosman@gmail.com>
2012-01-16 16:18:21 -0500

    initial commit. Does not have any expiry logic, and does not play well with browsers without localstorage. Really just alpha for now

---
+++ BackboneLocalCache.js
@@ -0,0 +1,87 @@
+
+// A simple module to add a localstorage cache to `Backbone.sync`.
+// objects you have already fetched are retrieved from the local
+// cache. If there isn't a cache, it falls back to 'Backbine.sync'
+// which does not need to be the default implementation
+
+// to use, extend Backbone.CachedModel instead of Backbone.Model
+// for your models. Ensure you define a unique 'cacheName' for each
+// model
+
+// set of helper funtions
+Backbone.BackboneLocalCache = Backbone.BackboneLocalCache || {};
+
+Backbone.BackboneLocalCache.generateKey = function(model)
+  {
+    if (model == null || model.cacheName == null || model.cacheName.length < 1 || model.id == null || model.id <= 0)
+    {
+      // can not generate a ID for this model
+      return null;
+    }
+    return "BACKBONE_CACHE::" + model.cacheName + "::" + model.id;
+  };
+
+Backbone.BackboneLocalCache.syncAndCacheModel = function(method, model, options)
+  {
+    var cacheKey = Backbone.BackboneLocalCache.generateKey(model);
+
+    if (cacheKey != null)
+    {
+      var superSuccess = options.success;
+      options.success = function(modelAttr, resp)
+      {
+        localStorage.setItem(cacheKey, JSON.stringify(modelAttr));
+        superSuccess(modelAttr, resp);
+      };
+    }
+
+    return Backbone.sync(method, model, options);
+  };
+
+// new sync function, used by CachedModel. Relies on Backbone.sync so don't set 
+// Backbone.sync to this unless you want infinite recursion
+Backbone.cachedSync = function(method, model, options) {
+  var cacheKey = Backbone.BackboneLocalCache.generateKey(model);
+
+  if (method == "read")
+  {
+    // first check the cache
+    var cached = localStorage.getItem(cacheKey);
+    if (cached != null )
+    {
+      var cachedModel = JSON.parse(cached);
+      options.success(cachedModel, "success");
+      return null;
+    }
+    else 
+    {
+      return Backbone.BackboneLocalCache.syncAndCacheModel(method, model, options);
+    }
+  }
+  else if (method == "create" || method == "update")
+  {
+    return Backbone.BackboneLocalCache.syncAndCacheModel(method, model, options);
+  }
+  else if (method == "delete")
+  {
+    localStorage.removeItem(cacheKey);
+    return Backbone.sync(method, model, options);
+  }
+};
+
+// extend this to get all the localstorage goodness
+Backbone.CachedModel = Backbone.Model.extend({
+  // TODO P0 initialize - add to cache when created if it has an id attribute
+  initialize: function (attributes)
+  {
+    if (this.cacheName == null || this.cacheName.length < 1)
+    {
+      // a cached model requires a cacheName. revert to a normal model
+      this.sync = null;
+    }
+    Backbone.Model.prototype.initialize.call(attributes);
+  },
+
+  sync: Backbone.cachedSync,
+});
+
